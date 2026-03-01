0ba65768212cf1fb74dd5c86e5a4d62296677a83
savereks <savasovamos@gmail.com>
2026-03-01 19:24:31 +0300

    Added static folder and modified general context

--- core/views.py
+++ core/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 
-def general_context():
+
+def general_context(request):
+    """ Создает общий контекст """
     context = {
         "menu": [
             ["Задать вопрос", "/create_question"],
         ]
     }
+    if request.user.is_authenticated:
+        context['menu'].append(['Профиль', '/accounts/profile'])
+        context['menu'].append(['Logout', '/accounts/logout'])
+    else:
+        context['menu'].append(['Логин', '/accounts/login'])
     return context
 
 # Create your views here.
 def main(request):
-    return render(request, 'index.html', general_context())
+    return render(request, 'index.html', general_context(request))
 
 def calculator(request):
-    return render(request, 'calc.html', general_context())
+    return render(request, 'calc.html', general_context(request))
