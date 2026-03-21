5061e89611ee9ca847884d9905e34244263486d1
dsf <sdf@gmail.com>
2026-03-21 15:26:36 +0300

    добавлены модели Comment, Vote

--- core/models.py
+++ core/models.py
@@ -20,4 +20,28 @@ class Answer(models.Model):
     def __str__(self):
         return self.text
 
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True, blank=True)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True, blank=True)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = [
+            ['user', 'question'],
+            ['user', 'answer']
+        ]
+    def __str__(self):
+        return self.user.username
+
 # Create your models here.
