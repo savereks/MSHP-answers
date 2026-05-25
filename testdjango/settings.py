543c3eacc1193aad7a054745608c0be4c1324043
dianacherr116-ui <dianacherr116@gmail.com>
2026-05-26 00:24:47 +0300

    switch database to PostgreSQL

--- testdjango/settings.py
+++ testdjango/settings.py
@@ -72,8 +72,12 @@ WSGI_APPLICATION = 'testdjango.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'mshp_answers',
+        'USER': 'postgres',
+        'PASSWORD': '123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
